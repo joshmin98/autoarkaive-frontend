e2a8cf73ec98b1169827df43323c9f7e0222be4d
Joshua Min <minje@usc.edu>
2018-11-18 14:18:33 -0800

    Starting on login page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+
+};
 
 class Login extends Component {
   render() {
     return(
-      <p>Login page</p>
+      <Typography component='h1'>
+        TEST
+      </Typography>
     );
   }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
