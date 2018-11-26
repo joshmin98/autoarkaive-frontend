5f43c5bba44e4d9b4db53ecbdd078894488cbf26
Joshua Min <minje@usc.edu>
2018-11-25 20:50:25 -0800

    Maybe ready?

---
+++ build/precache-manifest.7932c3982e567202f6325d54272176c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef7ae663a47b3dd3c76a",
+    "url": "/static/js/main.ef7ae663.chunk.js"
+  },
+  {
+    "revision": "7b438d64d60c86eaaa16",
+    "url": "/static/js/1.7b438d64.chunk.js"
+  },
+  {
+    "revision": "ef7ae663a47b3dd3c76a",
+    "url": "/static/css/main.9eae31c6.chunk.css"
+  },
+  {
+    "revision": "d01a8a2d5385613fed86f37074bc7f58",
+    "url": "/index.html"
+  }
+];
